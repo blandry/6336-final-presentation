7503233ddded74139cef7314018d4d3f3a2f9e8b
Benoit Landry <landry@mit.edu>
2013-12-03 19:57:39 -0500

    final simulations

--- visualize3D.m
+++ visualize3D.m
@@ -2,41 +2,39 @@ function [ ] = visualize3D( x, t )
 %VISUALIZE3D Summary of this function goes here
 %   Detailed explanation goes here
 
+% to get some sensible boundaries from another simulation
 tdata = dlmread('/usr/local/MATLAB/R2013b/toolbox/aero/astdemos/asthl20log.csv',',',1,0);
-N = size(tdata,1);
+
+N = size(x,2);
+
+xtraj = x(1,:)';
+ytraj = x(2,:)';
+
+altmin = 0;
+ymin = min(ytraj);
+ymax = max(ytraj);
+altmax = min(10*ymax,10000);
+ytrajn = (ytraj-ymin)/(ymax-ymin);
+alt = ytrajn*(altmax-altmin)+altmin;
+
+coordsref = convang(tdata(:,[3 2]), 'deg', 'rad');
+coordsstart = coordsref(1,:);
+coordsend = coordsref(end,:);
+xmin = min(xtraj);
+xmax = max(xtraj);
+xtrajn = (xtraj-xmin)/(xmax-xmin);
+coords = xtrajn*(coordsend-coordsstart)+repmat(coordsstart,N,1);
+
 pitch = zeros(N,1);
-for i=1:N-1
-    u = [x(1,i);x(2,i)];
-    v = [x(1,i+1);x(2,i+1)];
-    d = v-u;
-    pitch(i) = atan(d(2)/d(1));;
+for i=2:N-1
+    hdist = pos2dist(coords(i-1,1),coords(i-1,2),coords(i,1),coords(i,2),2)*1000;
+    vdist = alt(i)-alt(i-1);
+    pitch(i) = convang(atan(vdist/hdist),'deg','rad');
 end
-pitch(N) = pitch(N-1);
 
-xtraj = x(1,1:N)';
-ytraj = x(2,1:N)';
-t = t(1:N);
+eulang = [zeros(N,1),pitch,2.0577*ones(N,1)];
 
-% could just be a constant?
-lat = convang(tdata(:,3),'deg','rad');
-
-lng0 = convang(tdata(1,2),'deg','rad');
-lngf = convang(tdata(end,2), 'deg','rad');
-lng0s = xtraj(1);
-lngfs = xtraj(end);
-xtrajn = (xtraj-lng0s)/(lngfs-lng0s);
-lng = xtrajn*(lngf-lng0)+lng0;
-
-alt0 = tdata(1,4);
-altf = tdata(end,4);
-alt0s = ytraj(1);
-altfs = ytraj(end);
-ytrajn = (ytraj-alt0s)/(altfs-alt0s);
-alt = ytrajn*(altf-alt0)+alt0;
-
-%eulang = convang(tdata(:,5:7),'deg','rad');
-eulang = [zeros(N,1),pitch,zeros(N,1)];
-
+% scales time to make it play nicely
 t0 = tdata(1,1);
 tf = tdata(end,1);
 t0s = t(1);
@@ -44,7 +42,7 @@ tfs = t(end);
 tn = (t-t0s)/(tfs-t0s);
 time = tn*(tf-t0)+t0;
 
-ts = timeseries([lat,lng,alt,eulang],time);
+ts = timeseries([coords,alt,eulang],time);
 
 h = Aero.FlightGearAnimation;
 h.TimeseriesSourceType = 'Timeseries';
@@ -59,11 +57,11 @@ h.DestinationPort = '5502';
 h.AirportId = 'KSFO';
 h.RunwayId = '10L';
 h.InitialAltitude = alt(1);
-h.InitialHeading = 113;
+h.InitialHeading = 2.0577;
 h.OffsetDistance = 4.72;
 h.OffsetAzimuth = 0;
 
-h.TimeScaling = 5;
+h.TimeScaling = 10;
 
 GenerateRunScript(h);
 system('. runfg.bat &');
--- visualizeTrapezoidal.m
+++ visualizeTrapezoidal.m
@@ -7,9 +7,11 @@ dt = t(2)-t(1);
 x0 = [0;0;5;0];
 
 % input of the plane - thrust vector [dx/dt;dy/dt]
+%u = zeros(2,N);
 u = [repmat([1;0],1,N/8),zeros(2,N/4),repmat([1;20],1,N/8),zeros(2,N/2)];
+%u = [zeros(2,N/2),repmat([10;0],1,N/8),zeros(2,N/2-N/8)];
 
 x = trapruleplane(x0,u,tf,dt);
 
-%visualize2D(x);
+visualize2D(x);
 visualize3D(x,t);
